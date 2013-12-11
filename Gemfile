1d01d2e42b0882a2041c438336fb50ba39d86bbf
Jason Butz <jason.butz@inin.com>
2013-12-11 18:05:40 -0500

    Update contact form, and other things

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'less'
-gem 'therubyracer'
-gem 'coffee-script'
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,75 +1,76 @@
-/*
-$('form#contactForm').submit (e)->
-	console.log 'It is running...'
-	e.stopPropagation()
-	status = true
-	# Remove existing messages
-	$('#contactForm small.error').remove()
-	$('div.contactAlerts').empty()
-	$('#contactForm .error').removeClass('error')
-
-	$author       = $('#contactForm input[name="message[author]"]')
-	$author_email = $('#contactForm input[name="message[author_email]"]')
-	$content      = $('#contactForm textarea[name="message[content]"]')
-
-	# Validate fields
-	if $author.val().trim() is ""
-		status = false
-		$author.prev('label').addClass('error')
-		$author.addClass('error')
-		$author.after('<small class="error">This field is required.</small>')
-
-	if $author_email.val().trim() is ""
-		status = false
-		$author_email.prev('label').addClass('error')
-		$author_email.addClass('error')
-		$author_email.after('<small class="error">This field is required.</small>')
-
-	if $content.val().trim() is ""
-		status = false;
-		$content.prev('label').addClass('error')
-		$content.addClass('error')
-		$content.after('<small class="error">This field is required.</small>')
-
-	return false unless status
-
-	# Hide the button, show the spinner
-	$('#contactForm input[type="submit"]').hide()
-	$('#contactForm img.loadSpinner').show()
-	# Send the AJAX
-	$.ajax
-		type: "POST"
-		#url: "http://127.0.0.1:3000/messages.json"
-		url: "http://jasonbutzinfo.herokuapp.com/messages.json"
-		cache: false
-		contentType: "application/json; charset=utf-8",
-		dataType: "json",
-		data: JSON.stringify
-			message:
-				author: $author.val()
-				author_email: $author_email.val()
-				content: $content.val()
-
-	.fail (jqXHR, textStatus, errorThrown)->
-		$('div.contactAlerts').append('<div class="alert-box alert">Error: '+errorThrown+'</div>')
-
-	.done (data, textStatus, jqXHR)->
-		if data.error
-			$('div.contactAlerts').append('<div class="alert-box alert">Error: '+data.error+'</div>')
-
-		if data.success
-			$('div.contactAlerts').append('<div class="alert-box success">'+data.success+'</div>')
-			$('#contactForm input[type!="submit"], #contactForm textarea').val("")
-			$('#recaptcha_reload_btn,#recaptcha_reload').click()
-	.always ->
-		$('#contactForm input[type="submit"]').show()
-		$('#contactForm img.loadSpinner').hide()
-
-	return false
-*/
-
-
 (function() {
+  $('form#contactForm').submit(function(e) {
+    var $author, $author_email, $content, status;
+    if ($('#contactForm input[name="try_ajax"]').val() !== "true") {
+      return true;
+    }
+    e.stopPropagation();
+    status = true;
+    $('#contactForm small.error').remove();
+    $('div.contactAlerts').empty();
+    $('#contactForm .error').removeClass('error');
+    $author = $('#contactForm input[name="message[author]"]');
+    $author_email = $('#contactForm input[name="message[author_email]"]');
+    $content = $('#contactForm textarea[name="message[content]"]');
+    if ($author.val().trim() === "") {
+      status = false;
+      $author.prev('label').addClass('error');
+      $author.addClass('error');
+      $author.after('<small class="error">This field is required.</small>');
+    }
+    if ($author_email.val().trim() === "") {
+      status = false;
+      $author_email.prev('label').addClass('error');
+      $author_email.addClass('error');
+      $author_email.after('<small class="error">This field is required.</small>');
+    }
+    if ($content.val().trim() === "") {
+      status = false;
+      $content.prev('label').addClass('error');
+      $content.addClass('error');
+      $content.after('<small class="error">This field is required.</small>');
+    }
+    if (!status) {
+      return false;
+    }
+    $('#contactForm input[type="submit"]').hide();
+    $('#contactForm img.loadSpinner').show();
+    $.ajax({
+      type: "POST",
+      url: "https://jasonbutzinfo.herokuapp.com/messages.json",
+      cache: false,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify({
+        message: {
+          author: $author.val(),
+          author_email: $author_email.val(),
+          content: $content.val()
+        }
+      })
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      return $('div.contactAlerts').append('<div class="alert-box alert">Error: ' + jqXHR.responseJSON.errors + '</div>');
+    }).done(function(data, textStatus, jqXHR) {
+      debugger;
+      if (data.errors) {
+        return $('div.contactAlerts').append('<div class="alert-box alert">Error: ' + data.errors + '</div>');
+      } else {
+        $('div.contactAlerts').append('<div class="alert-box success">' + data.success + '</div>');
+        return $('#contactForm input[type!="submit"], #contactForm textarea').val("");
+      }
+    }).always(function() {
+      $('#contactForm input[type="submit"]').show();
+      return $('#contactForm img.loadSpinner').hide();
+    });
+    setTimeout(function() {
+      if ($('div.contactAlerts div').length === 0) {
+        $('div.contactAlerts').append('<div class="alert-box info">It looks like you may not be able to use some of the nifty things I have done on this site. Please click Submit again, this time a different method will be used to send your message.</div>');
+        return $('#contactForm input[name="try_ajax"]').val('false');
+      }
+    }, 10000);
+    return false;
+  });
+
   $(document).ready(function() {
     var params;
     params = $.parseParams(window.location.search);
