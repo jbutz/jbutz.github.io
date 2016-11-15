221618de71b40713c9abacb4acdf0e508db0c3f0
Jason Butz <jason.butz@inin.com>
2016-11-15 14:13:57 -0500

    Remove Old Files

---
+++ assets/js/plugins.js
@@ -0,0 +1,50 @@
+// http://stackoverflow.com/a/1131658/376458
+/*jQuery.unparam = function (value) {
+    var
+    // Object that holds names => values.
+    params = {},
+    // Get query string pieces (separated by &)
+    pieces = value.split('&'),
+    // Temporary variables used in loop.
+    pair, i, l;
+
+    // Loop through query string pieces and assign params.
+    for (i = 0, l = pieces.length; i < l; i++) {
+        pair = pieces[i].split('=', 2);
+        // Repeated parameters with the same name are overwritten. Parameters
+        // with no value get set to boolean true.
+        params[decodeURIComponent(pair[0])] = (pair.length == 2 ?
+            decodeURIComponent(pair[1].replace(/\+/g, ' ')) : true);
+    }
+
+    return params;
+};*/
+// https://gist.github.com/kares/956897
+(function($) {
+    var re = /([^&=]+)=?([^&]*)/g;
+    var decode = function(str) {
+        return decodeURIComponent(str.replace(/\+/g, ' '));
+    };
+    $.parseParams = function(query) {
+        var params = {}, e;
+        if (query) {
+            if (query.substr(0, 1) == '?') {
+                query = query.substr(1);
+            }
+
+            while (e = re.exec(query)) {
+                var k = decode(e[1]);
+                var v = decode(e[2]);
+                if (params[k] !== undefined) {
+                    if (!$.isArray(params[k])) {
+                        params[k] = [params[k]];
+                    }
+                    params[k].push(v);
+                } else {
+                    params[k] = v;
+                }
+            }
+        }
+        return params;
+    };
+})(jQuery);
